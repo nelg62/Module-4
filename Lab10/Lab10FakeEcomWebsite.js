83bb9ef3ff35799789ed59bf67c73f84377de136
nelg62 <glen.harding.nz@gmail.com>
2024-04-17 12:01:51 +1200

    add coments need to create dropdown sort

--- Lab10/Lab10FakeEcomWebsite.js
+++ Lab10/Lab10FakeEcomWebsite.js
@@ -1,3 +1,4 @@
+// class creator for products
 class Items {
     constructor(category, description, id, image, price, rating, title) {
         this.category = category
@@ -9,14 +10,22 @@ class Items {
         this.title = title
     }
 }
-
+// array to store products to update and diplay
 const newItem = []
 
+// array to store caregorys for drpdown sort
+const tempCategorys = []
+
+// axios get fake stor api data 
 axios.get('https://fakestoreapi.com/products').then((response) => {
-    console.log(response.data)
+    // console.log(response.data)
+
+    // sort through data
 for (const iterator of response.data) {
+    // push items to the new item array using the class constrructor 
     newItem.push(new Items(iterator.category, iterator.description, iterator.id, iterator.image, iterator.price, iterator.rating, iterator.title))
 
+    // use upadte data on template with api data
     const template = document.getElementById('list-template').content.cloneNode(true)
 template.querySelector(".card-img-top").src = iterator.image
 template.querySelector(".card-title").innerHTML = iterator.title
@@ -26,11 +35,25 @@ template.querySelector(".cardid").innerHTML = iterator.id
 template.querySelector(".cardprice").innerHTML = `$${iterator.price}`
 template.querySelector(".cardratingCount").innerHTML = `Number of Ratings: ${iterator.rating.count}`
 template.querySelector(".cardratingRate").innerHTML = iterator.rating.rate
-    console.log(newItem)
-    console.log(template)
+    // console.log(newItem)
+    // console.log(template)
 
+    // show the data for the template in html
     document.querySelector("#main").appendChild(template)
+    
+    // push caterories to the temp array tempcaregories
+        tempCategorys.push(iterator.category)
+        // console.log(iterator.category)
+        
+    
 }
 
+// create a set to remove duplicates in the temp categiries 
+const setArray = new Set(tempCategorys)
+                console.log(...setArray)
     // use this JSON to find and set correct option data for the chart
+ 
 })
+
+
+
